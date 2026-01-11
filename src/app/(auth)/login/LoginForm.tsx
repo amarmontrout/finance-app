e7670e066dfad9f4159e08164120c530402c6ed8
amarmontrout <numbest_rougher0y@icloud.com>
2026-01-10 19:17:43 -0700

    Simple login form

---
+++ src/app/(auth)/login/LoginForm.tsx
@@ -0,0 +1,98 @@
+"use client"
+
+import { FlexColWrapper } from "@/components/Wrappers"
+import { accentColorSecondary } from "@/globals/colors"
+import { 
+  Box, 
+  Button, 
+  Card, 
+  CardContent, 
+  FormControl, 
+  InputLabel, 
+  OutlinedInput, 
+  Typography 
+} from "@mui/material"
+import { useState } from "react"
+
+type CredType = {
+  username: string
+  password: string
+}
+
+const CRED_INIT = {
+  username: "",
+  password: ""
+}
+
+const LoginForm = () => {
+  const [credentials, setCredentials] = useState<CredType>(CRED_INIT)
+
+  return (
+    <Box
+      height={"100%"}
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Card
+        style={{
+          borderRadius: "15px",
+          margin: "0 auto"
+        }}
+      >
+        <CardContent>
+          <FlexColWrapper gap={5}>
+            <Typography textAlign={"center"}>
+              Please Sign In
+            </Typography>
+
+            <Box className="flex flex-col gap-3 m-auto">
+              <FormControl>
+                <InputLabel>Username</InputLabel>
+                <OutlinedInput
+                  label={"Username"}
+                  value={credentials.username}
+                  name={"username"}
+                  onChange={(e) => {
+                    setCredentials(prev => ({
+                      ...prev,
+                      username: e.target.value
+                    }))
+                  }}
+                  />
+              </FormControl>
+
+              <FormControl>
+                <InputLabel>Password</InputLabel>
+                <OutlinedInput
+                  label={"Password"}
+                  type={"password"}
+                  value={credentials.password}
+                  name={"password"}
+                  onChange={(e) => {
+                    setCredentials(prev => ({
+                      ...prev,
+                      password: e.target.value
+                    }))
+                  }}
+                  />
+              </FormControl>
+
+              <Button
+                variant={"contained"} 
+                onClick={() => {}}
+                disabled={!credentials.username || !credentials.password}
+                sx={{ backgroundColor: accentColorSecondary }}
+              >
+                {"Sign In"}
+              </Button>
+            </Box>
+          </FlexColWrapper>
+        </CardContent>
+      </Card>
+    </Box>
+  )
+}
+
+export default LoginForm
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material"
+import LoginForm from "./LoginForm"
 
 const Page = () => {
   return (
     <Box
       width={"100%"}
+      height={"100%"}
     >
-      Login Page
+      <LoginForm/>
     </Box>
   )
 }

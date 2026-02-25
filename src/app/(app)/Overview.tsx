08063eef3f98717fa281385c6c61d176299c8bbc
amarmontrout <numbest_rougher0y@icloud.com>
2026-02-24 22:37:41 -0600

    Condensed the Overview

--- src/app/(app)/Overview.tsx
+++ src/app/(app)/Overview.tsx
@@ -8,10 +8,8 @@ import { darkMode, lightMode } from "@/globals/colors"
 import { buildMultiColumnDataV2 } from "@/utils/buildChartData"
 import { getCurrentDateInfo } from "@/utils/helperFunctions"
 import { useTheme } from "next-themes"
-import { useMemo, useState } from "react"
+import { useMemo } from "react"
 import YearTotals from "./YearTotals"
-import YearNetCash from "./YearNetCash"
-import { Box, Tab, Tabs } from "@mui/material"
 
 const Overview = () => {
   const { incomeTransactionsV2, expenseTransactionsV2 } =
@@ -19,8 +17,6 @@ const Overview = () => {
   const { excludedSet } = useCategoryContext()
   const { currentYear, passedMonths } = getCurrentDateInfo()
   const { theme: currentTheme } = useTheme()
-
-  const [value, setValue] = useState(0)
 
   const lineColor =
     currentTheme === "light"
@@ -37,58 +33,16 @@ const Overview = () => {
     })
   }, [incomeTransactionsV2, expenseTransactionsV2])
 
-  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue)
-  }
-
-  const TabPanel = ({
-    children,
-    value,
-    index,
-    ...other
-  }: {
-    children?: React.ReactNode
-    index: number
-    value: number
-  }) => {
-    return (
-      <div hidden={value !== index} {...other}>
-        {value === index && <Box>{children}</Box>}
-      </div>
-    )
-  }
-
   return (
     <FlexColWrapper gap={2}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={value} onChange={handleChange}>
-          <Tab label="Annual Totals" />
-          <Tab label="Annual Net Cash" />
-        </Tabs>
-      </Box>
-
-      <TabPanel value={value} index={0}>
-        <YearTotals
-          currentYear={currentYear}
-          passedMonths={passedMonths}
-          currentTheme={currentTheme}
-          excludedSet={excludedSet}
-          incomeTransactionsV2={incomeTransactionsV2}
-          expenseTransactionsV2={expenseTransactionsV2}
-        />
-      </TabPanel>
-
-      <TabPanel value={value} index={1}>
-        <YearNetCash
-          currentYear={currentYear}
-          passedMonths={passedMonths}
-          currentTheme={currentTheme}
-          excludedSet={excludedSet}
-          incomeTransactionsV2={incomeTransactionsV2}
-          expenseTransactionsV2={expenseTransactionsV2}
-        />
-      </TabPanel>
-
+      <YearTotals
+        currentYear={currentYear}
+        passedMonths={passedMonths}
+        currentTheme={currentTheme}
+        excludedSet={excludedSet}
+        incomeTransactionsV2={incomeTransactionsV2}
+        expenseTransactionsV2={expenseTransactionsV2}
+      />
       <LineChart multiColumnData={chartDataV2} lineColors={lineColor} />
     </FlexColWrapper>
   )
--- src/app/(app)/YearTotals.tsx
+++ src/app/(app)/YearTotals.tsx
@@ -1,8 +1,13 @@
 import ColoredInfoCard from "@/components/ColoredInfoCard"
 import ShowCaseCard from "@/components/ShowCaseCard"
 import { FlexColWrapper } from "@/components/Wrappers"
+import { getNetCashFlow } from "@/utils/financialFunctions"
 import { getYearUpToMonthTotalV2 } from "@/utils/getTotals"
-import { getCardColor } from "@/utils/helperFunctions"
+import {
+  cleanNumber,
+  getCardColor,
+  getSavingsHealthState,
+} from "@/utils/helperFunctions"
 import { TransactionTypeV2 } from "@/utils/type"
 
 const YearTotals = ({
@@ -33,6 +38,24 @@ const YearTotals = ({
     expenseTransactionsV2,
     excludedSet,
   )
+  const annualIncome = getYearUpToMonthTotalV2(
+    currentYear,
+    passedMonths,
+    incomeTransactionsV2,
+    excludedSet,
+  )
+  const annualExpense = getYearUpToMonthTotalV2(
+    currentYear,
+    passedMonths,
+    expenseTransactionsV2,
+    excludedSet,
+  )
+  const annualNetIncome = getNetCashFlow(annualIncome, annualExpense)
+  const savingsHealthState = getSavingsHealthState(
+    cleanNumber(annualNetIncome),
+    cleanNumber(annualIncome),
+  )
+  const savingsColor = getCardColor(currentTheme, savingsHealthState)
 
   return (
     <ShowCaseCard title={`${currentYear} Totals`}>
@@ -46,6 +69,11 @@ const YearTotals = ({
           cardColors={defaultCardColor}
           info={`$${totalExpenses}`}
           title={`${currentYear} Total Expenses`}
+        />
+        <ColoredInfoCard
+          cardColors={savingsColor}
+          info={`Net Cash: $${annualNetIncome}`}
+          title={`Net Cash Rating: ${savingsHealthState}`}
         />
       </FlexColWrapper>
     </ShowCaseCard>

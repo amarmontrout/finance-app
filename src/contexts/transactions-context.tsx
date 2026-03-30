b2e17affb6019a9f3f06a84610eee11f54449ea4
amarmontrout <numbest_rougher0y@icloud.com>
2026-03-30 15:05:03 -0600

    More clean up

--- src/contexts/transactions-context.tsx
+++ src/contexts/transactions-context.tsx
@@ -1,24 +1,9 @@
-import {
-  getBudget,
-  getExpenses,
-  getIncome,
-  getTransactions,
-} from "@/app/api/Transactions/requests"
+import { getTransactions } from "@/app/api/Transactions/requests"
 import { useUser } from "@/hooks/useUser"
-import {
-  BudgetTransactionType,
-  NewTransactionType,
-  TransactionType,
-} from "@/utils/type"
+import { NewTransactionType } from "@/utils/type"
 import { createContext, useContext, useEffect, useState } from "react"
 
 type TransactionsContextType = {
-  incomeTransactions: TransactionType[]
-  refreshIncomeTransactions: () => void
-  expenseTransactions: TransactionType[]
-  refreshExpenseTransactions: () => void
-  budgetTransactions: BudgetTransactionType[]
-  refreshBudgetTransactions: () => void
   isLoading: boolean
   transactions: NewTransactionType[]
   refreshTransactions: () => Promise<void>
@@ -52,55 +37,10 @@ export const TransactionProvider = (props: { children: React.ReactNode }) => {
     })
     setTransactions(result ?? [])
   }
-  //////////////////////////////////////////////////////////////////////////////
-  const [incomeTransactions, setIncomeTransactions] = useState<
-    TransactionType[]
-  >([])
-  const [expenseTransactions, setExpenseTransactions] = useState<
-    TransactionType[]
-  >([])
-  const [budgetTransactions, setBudgetTransactions] = useState<
-    BudgetTransactionType[]
-  >([])
-  const refreshIncomeTransactions = async () => {
-    if (!user) {
-      return
-    }
-    console.log("Pulling Income Transactions...")
-    const incomeResult = await getIncome({
-      userId: user.id,
-    })
-    setIncomeTransactions(incomeResult ?? [])
-  }
-  const refreshExpenseTransactions = async () => {
-    if (!user) {
-      return
-    }
-    console.log("Pulling Expenses Transactions...")
-    const expenseResult = await getExpenses({
-      userId: user.id,
-    })
-    setExpenseTransactions(expenseResult ?? [])
-  }
-  const refreshBudgetTransactions = async () => {
-    if (!user) {
-      return
-    }
-    console.log("Pulling Budget Transactions...")
-    const budgetResult = await getBudget({
-      userId: user.id,
-    })
-    setBudgetTransactions(budgetResult ?? [])
-  }
   const loadTransactions = async () => {
     if (!user) return
     setIsLoading(true)
-    await Promise.all([
-      refreshTransactions(),
-      refreshIncomeTransactions(),
-      refreshExpenseTransactions(),
-      refreshBudgetTransactions(),
-    ])
+    await Promise.all([refreshTransactions()])
     setIsLoading(false)
   }
 
@@ -113,12 +53,6 @@ export const TransactionProvider = (props: { children: React.ReactNode }) => {
   return (
     <TransactionContext.Provider
       value={{
-        incomeTransactions,
-        refreshIncomeTransactions,
-        expenseTransactions,
-        refreshExpenseTransactions,
-        budgetTransactions,
-        refreshBudgetTransactions,
         isLoading,
         transactions,
         refreshTransactions,
